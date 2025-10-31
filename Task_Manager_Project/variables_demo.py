cce9fc85e2dd3020d0764cba816361e136408219
khushilalwanii <lalwaniikhushi@gmail.com>
2025-11-01 02:05:31 +0530

    initial commit with practice files

---
+++ Task_Manager_Project/variables_demo.py
@@ -0,0 +1,13 @@
+# Variables and Data Types Example
+
+name = "Khushi"
+age = 20
+is_student = True
+hobbies = ["Dance", "Video Editing", "Coding"]
+details = {"city": "Bangalore", "role": "Software Engineer Fresher"}
+
+print("Name:", name)
+print("Age:", age)
+print("Student:", is_student)
+print("Hobbies:", hobbies)
+print("Details:", details)
